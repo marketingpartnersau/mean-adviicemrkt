54bd112b18fb48a583760c9bd845448617ce621d
Harley Alexander <iharley@baffleinc.com>
2014-11-21 16:26:43 +1100

    quick clean up

--- client/app/pitches/Pitch/Pitch.service.js
+++ client/app/pitches/Pitch/Pitch.service.js
@@ -1,16 +1,32 @@
 'use strict';
 
 angular.module('adviicemrktApp')
-  .factory('Pitch', function () {
-    // Service logic
-    // ...
+  .factory('Pitch', function ($http, $q) {
+    
+    var API = '/api/pitches';
 
-    var meaningOfLife = 42;
+    var Pitch = function(data){
+      var self = this;
 
-    // Public API here
-    return {
-      someMethod: function () {
-        return meaningOfLife;
-      }
+      _.each(data, function(v, k){
+        self[k] = v;
+      });
     };
+
+    Pitch.prototype.$create = function(){
+      var def = $q.defer();
+      var self = this;
+
+      $http.post(API, self)
+      .then(function(result){
+        def.resolve(result);
+      }, function(err){
+        def.reject(err);
+      });
+
+      return def.promise;
+    };
+
+    return Pitch;
+
   });
